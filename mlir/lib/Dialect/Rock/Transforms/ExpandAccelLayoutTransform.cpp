b6f8105f90e6cea78f38a734c7cbc08b51c9aec5
Daniel Hernandez <danherna@amd.com>
2025-08-05 09:09:47 +0000

    fix tests

--- mlir/lib/Dialect/Rock/Transforms/ExpandAccelLayoutTransform.cpp
+++ mlir/lib/Dialect/Rock/Transforms/ExpandAccelLayoutTransform.cpp
@@ -154,8 +154,10 @@ struct ExpandAccelLayout
       return b.notifyMatchFailure(op, "wrong output type rank");
 
     int64_t g = outputType.getShape()[0];
-    int64_t d = kBlockFirst ? outputType.getShape()[1] : outputType.getShape()[2];
-    int64_t k = kBlockFirst ? outputType.getShape()[2] : outputType.getShape()[1];
+    int64_t d =
+        kBlockFirst ? outputType.getShape()[1] : outputType.getShape()[2];
+    int64_t k =
+        kBlockFirst ? outputType.getShape()[2] : outputType.getShape()[1];
     int64_t kPerBlock = kPackPerBlock * kPack;
     if (d % dPerBlock != 0 || k % kPerBlock != 0)
       return b.notifyMatchFailure(
